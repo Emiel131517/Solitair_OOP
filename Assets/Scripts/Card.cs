a161382ee02ef3b06da7c236136a47eed70a74a0
Emiel131517 <84375895+Emiel131517@users.noreply.github.com>
2022-05-11 14:13:03 +0200

    starting on card

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    private int rank;
+    private char suit;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    private List<Card> cards;
+    void Start()
+    {
+        cards = new List<Card>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
