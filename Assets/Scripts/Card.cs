03a98e97434b60aebdcbc64937e81cf4395a81e6
Emiel131517 <84375895+Emiel131517@users.noreply.github.com>
2022-06-01 11:58:35 +0200

    added card deck and deck shuffle.
    added 52 cards to the card deck. 13 of each rank. Deck can also shuffle

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,19 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Card : MonoBehaviour
+namespace Solitair
 {
-    private int rank;
-    private char suit;
-    void Start()
+    public enum Rank { HEART, DIAMOND, CLUB, SPADE };
+    public class Card
     {
-        
-    }
+        private int value;
+        private Rank rank;
+        private Sprite front;
+        private Sprite back;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public Card(int v, Rank r)
+        {
+            value = v;
+            rank = r;
+        }
+        public void PrintCardInfo()
+        {
+            Debug.Log(value + ": " + rank);
+        }
     }
 }
--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -1,18 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class Deck : MonoBehaviour
+namespace Solitair
 {
-    private List<Card> cards;
-    void Start()
+    public class Deck : MonoBehaviour
     {
-        cards = new List<Card>();
-    }
+        private List<Card> deck;
+        void Start()
+        {
+            deck = new List<Card>();
+            GenerateDeck();
+            ShuffleDeck(deck);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        void Update()
+        {
+
+        }
+        // Generate a deck
+        private void GenerateDeck()
+        {
+            for (int i = 0; i < 52; i++)
+            {
+                // Add card rank
+                Rank r = (Rank)Enum.ToObject(typeof(Rank), i / 13);
+                // Create a card with the value and the rank
+                Card c = new Card(i % 13, r);
+                // Add the card to the list
+                deck.Add(c);
+            }
+        }
+
+        // Fisher-Yates shuffle
+        private List<Card> ShuffleDeck (List<Card> list)
+        {
+            for (int i = list.Count - 1; i > 0; i--)
+            {
+                int rnd = UnityEngine.Random.Range(0, list.Count);
+                Card c = list[i];
+                list[i] = list[rnd];
+                list[rnd] = c;
+            }
+
+            // Print list
+            for (int i = 0; i < list.Count; i++)
+            {
+                list[i].PrintCardInfo();
+            }
+            return list;
+        }
     }
 }
