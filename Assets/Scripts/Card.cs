861c2c9b15c24673dea70c021fdb3e8b6841f62f
Emiel131517 <84375895+Emiel131517@users.noreply.github.com>
2022-06-02 15:06:48 +0200

    Creating cards
    Creating 52 named gameobjects when the game starts

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -6,21 +6,31 @@ using UnityEngine;
 namespace Solitair
 {
     public enum Rank { HEART, DIAMOND, CLUB, SPADE };
-    public class Card
+    public class Card : MonoBehaviour
     {
+        private SpriteRenderer sprRend;
+        private GameObject obj;
         private int value;
         private Rank rank;
-        private Sprite front;
-        private Sprite back;
 
         public Card(int v, Rank r)
         {
             value = v;
             rank = r;
+
+            obj = new GameObject(value.ToString() + rank.ToString());
+            obj.AddComponent<Card>();
+            obj.AddComponent<SpriteRenderer>();
+
+            sprRend = GetComponent<SpriteRenderer>();
         }
         public void PrintCardInfo()
         {
             Debug.Log(value + ": " + rank);
+        }
+        private void AttachSprites()
+        {
+
         }
     }
 }
