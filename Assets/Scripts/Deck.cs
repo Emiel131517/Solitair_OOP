c8908db851196d074b6bd2d684c019a3041b8e38
Emiel131517 <84375895+Emiel131517@users.noreply.github.com>
2022-06-07 11:37:57 +0200

    generating GameObjects
    Generating the random deck as GameObjects

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -8,13 +8,12 @@ namespace Solitair
     public class Deck : MonoBehaviour
     {
         public List<Card> deck;
-        [SerializeField]
-        private Card prefab;
         void Start()
         {
             deck = new List<Card>();
             GenerateDeck();
             Shuffle(deck);
+            CreateObjects();
 
             // Print list
             for (int i = 0; i < deck.Count; i++)
@@ -27,30 +26,39 @@ namespace Solitair
         {
 
         }
-        // Generate a deck
+        // Generate a deck        
         private void GenerateDeck()
         {
             for (int i = 0; i < 52; i++)
             {
-                // Add card rank
                 Suit s = (Suit)Enum.ToObject(typeof(Suit), i / 13);
+                int v = i % 13;
+                Card c  = new Card();
+                c.SetCardStats(v, s);
+                deck.Add(c);
+            }
+
+            // return deck;
+        }
+        private void CreateObjects()
+        {
+            foreach(Card card in deck)
+            {
+                Card prefab = Resources.Load<Card>("Prefabs/Card");
+                prefab.value = card.value;
+                prefab.suit = card.suit;
                 Instantiate(prefab, transform.position, Quaternion.identity);
-                // Create a card with the value and the rank
-                prefab.SetCardStats(i % 13, s);
-                // Add the card to the list
-                deck.Add(prefab);
             }
         }
-
         // Fisher-Yates shuffle
         private List<T> Shuffle<T> (List<T> list)
         {
             for (int i = list.Count - 1; i > 0; i--)
             {
                 int rnd = UnityEngine.Random.Range(0, list.Count);
-                T c = list[i];
+                T s = list[i];
                 list[i] = list[rnd];
-                list[rnd] = c;
+                list[rnd] = s;
             }
             return list;
         }
